29651623db1a874b9b416f9a2057cab26cecb6f5
obed-tc <castroobed53@gmail.com>
2024-01-21 12:28:18 -0400

    Se modifico el modelo de moneda

--- app/Moneda.php
+++ app/Moneda.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Moneda extends Model
 {
     protected $fillable =[
-        'idempresa', 'nombre', 'pais', 'simbolo', 'compra', 'venta', 'activo'
+        'idempresa', 'nombre', 'pais', 'simbolo', 'tipo_cambio', 'activo'
     ];
     public function empresa(){
         return $this->belongsTo('App\Empresa');
