c205bcc440d1d9aabcdc6c10d0584a3352a7132d
MaebaFernandez <maebayorgely@gmail.com>
2024-05-20 16:22:53 -0400

    Update ImportarExcelProvedores.vue

--- resources/assets/js/components/compras/ImportarExcelProvedores.vue
+++ resources/assets/js/components/compras/ImportarExcelProvedores.vue
@@ -4,7 +4,7 @@
         <div class="modal-dialog modal-primary modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="myModalLabel"><i class="fa fa-upload"></i> Importar productos</h5>
+                    <h5 class="modal-title" id="myModalLabel"><i class="fa fa-upload"></i> Importar Provedores</h5>
                     <button type="button" class="close text-white" @click="cerrarModal()" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
