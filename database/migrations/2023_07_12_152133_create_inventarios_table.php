100b6a1553d292157312b3fc921906c2b69297ca
MaebaFernandez <maebayorgely@gmail.com>
2024-05-28 16:16:31 -0400

    Update 2023_07_12_152133_create_inventarios_table.php

--- database/migrations/2023_07_12_152133_create_inventarios_table.php
+++ database/migrations/2023_07_12_152133_create_inventarios_table.php
@@ -19,7 +19,7 @@ class CreateInventariosTable extends Migration
             $table->foreign('idalmacen')->references('id')->on('almacens');
             $table->integer('idarticulo')->unsigned();
             $table->foreign('idarticulo')->references('id')->on('articulos');
-            $table->date('fecha_vencimiento')->nullable();
+            $table->date('fecha_vencimiento')->nullable()->default('2099-01-01');
             $table->integer('saldo_stock'); //saldo_stock
             $table->timestamps();
         });
